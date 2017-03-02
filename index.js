0986d14ba7ef36f5d375d5c0c3be7dc1e6d86c95
Adrien Antoine <adriantoine@gmail.com>
2017-03-02 11:01:52 +0000

    Fixed unit tests

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ var hexToRgb = require('hex-to-rgb');
 
 module.exports = function (hex) {
 	var hsl = rgbToHsl.apply(rgbToHsl, hexToRgb(hex));
-	return [hsl[0], parseInt(hsl[1], 10), parseInt(hsl[2], 10)];
+	return [Math.round(hsl[0]), parseInt(hsl[1], 10), parseInt(hsl[2], 10)];
 };
--- test/colors.js
+++ test/colors.js
@@ -2,19 +2,19 @@ import test from 'ava';
 import hexToHsl from '../index';
 
 test('converts blue', t => {
-	t.deepEqual(hexToHsl('#0593FF'), [206, 100, 51]);
+	t.deepEqual(hexToHsl('#0593FF'), [206, 100, 50]);
 });
 
 test('converts green', t => {
-	t.deepEqual(hexToHsl('#00C176'), [157, 100, 38]);
+	t.deepEqual(hexToHsl('#00C176'), [157, 100, 37]);
 });
 
 test('converts orange', t => {
-	t.deepEqual(hexToHsl('#FF8A00'), [33, 100, 50]);
+	t.deepEqual(hexToHsl('#FF8A00'), [32, 100, 50]);
 });
 
 test('converts purple', t => {
-	t.deepEqual(hexToHsl('#A900C7'), [291, 100, 40]);
+	t.deepEqual(hexToHsl('#A900C7'), [291, 100, 39]);
 });
 
 test('converts red', t => {
